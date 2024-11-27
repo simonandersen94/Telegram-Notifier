6b60165aa87f677504ea9392eda7a9f80d3a5c95
Simon Andersen <10195871@ucn.dk>
2024-11-27 17:02:18 +0100

    Can now send message to Telegram when consumed new message from RabbitMQ

--- TelegramNofitier/TelegramNofitier/Rabbit_MQ/MessageConsumer.cs
+++ TelegramNofitier/TelegramNofitier/Rabbit_MQ/MessageConsumer.cs
@@ -7,15 +7,18 @@ using System.Text;
 using System.Threading.Tasks;
 using TelegramNofitier.Config;
 using TelegramNofitier.RabbitMQ.Interfaces;
+using TelegramNofitier.Telegram;
 
 namespace TelegramNofitier.RabbitMQ {
     public class MessageConsumer : IMessageConsumer {
         private readonly RabbitMQService _rabbitMQService;
         private readonly Config.Config _config;
+        private readonly TelegramService _telegramService;
 
-        public MessageConsumer(RabbitMQService rabbitMQService, Config.Config config) {
+        public MessageConsumer(RabbitMQService rabbitMQService, Config.Config config, TelegramService telegramService) {
             _rabbitMQService = rabbitMQService;
             _config = config;
+            _telegramService = telegramService;
         }
 
         public void StartConsuming() {
@@ -28,12 +31,14 @@ namespace TelegramNofitier.RabbitMQ {
             channel.BasicQos(0, 1, false);
 
             var consumer = new EventingBasicConsumer(channel);
-            consumer.Received += (sender, args) => {
+            consumer.Received += async (sender, args) => {
 
                 var body = args.Body.ToArray();
                 string message = Encoding.UTF8.GetString(body);
 
                 Console.WriteLine($"Message: {message}");
+                await _telegramService.SendMessageAsync(message);
+
                 channel.BasicAck(args.DeliveryTag, false);
             };
             string consumerTag = channel.BasicConsume(_config.RabbitMQ_QueueName, false, consumer);
